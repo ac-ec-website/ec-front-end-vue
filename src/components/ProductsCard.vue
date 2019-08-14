a2a29b3f18de56eb5b8f8fc7d78a78819fb83193
Lianginger <jackyliang444@hotmail.com.tw>
2019-08-14 16:08:07 +0800

    Modify ProductCard

--- src/components/ProductsCard.vue
+++ src/components/ProductsCard.vue
@@ -1,23 +1,39 @@
 <template>
   <div class="col-md-6 col-lg-4">
     <div class="card mb-4">
-      <img class="card-img-top" :src="product.image" alt="Card image cap" width="286px" height="180px" />
+      <div class="w-100">
+        <div
+          style="width: 100%;padding-bottom: 100%;background-position: center;background-repeat: no-repeat;background-size: cover"
+          :style="{ 'background-image': 'url(' + product.image + ')' }"
+        ></div>
+      </div>
 
       <div class="card-body">
-        <p class="card-text title-wrap">
-          <router-link :to="{ name: 'product', params: { id: product.id }}">{{ product.name }}</router-link>
-        </p>
-
-        <p class="card-text text-truncate">原價：{{ product.origin_price }}</p>
-        <p class="card-text text-truncate">特價：{{ product.sell_price }}</p>
+        <h4 class="card-text title-wrap font-weight-bold">
+          <router-link
+            :to="{ name: 'product', params: { productId: product.id }}"
+            class="stretched-link"
+          >{{ product.name }}</router-link>
+        </h4>
         <p class="card-text">{{ product.description }}</p>
+        <hr />
+        <span
+          class="float-right text-secondary card-text text-truncate"
+        >原價$ {{ product.origin_price }} 元</span>
+        <h5
+          class="card-text text-truncate font-weight-bold text-success"
+        >NT$ {{ product.sell_price }} 元</h5>
       </div>
 
       <div class="card-footer">
         <!-- <button v-if="product.isLiked" type="button" class="btn btn-danger btn-block" @click.stop.prevent="removeLike(product.id)">Unlike</button>
         <button v-else type="button" class="btn btn-primary btn-block" @click.stop.prevent="addLike(product.id)">Like</button>-->
 
-        <button type="button" class="btn btn-danger btn-block" @click.stop.prevent="addToCart()">放入購物車</button>
+        <button
+          type="button"
+          class="btn btn-danger btn-block"
+          @click.stop.prevent="addToCart()"
+        >放入購物車</button>
       </div>
     </div>
   </div>
