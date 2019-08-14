ef10cc0b8eed288527bb7a42fd093973484b90df
Lianginger <jackyliang444@hotmail.com.tw>
2019-08-14 16:42:23 +0800

    Modify Product page and Add ProductDetail

---
+++ src/components/ProductDetail.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1 class="py-3 font-weight-bold">{{product.name}}</h1>
+    <div class="row">
+      <div class="col-md-4 mb-5">
+        <div class="w-100">
+          <div
+            style="width: 100%;padding-bottom: 100%;background-position: center;background-repeat: no-repeat;background-size: cover"
+            :style="{ 'background-image': 'url(' + product.image + ')' }"
+          ></div>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <p>{{product.description}}</p>
+        <hr />
+        <span
+          class="float-right text-secondary card-text text-truncate"
+        >原價$ {{ product.origin_price }} 元</span>
+        <h5
+          class="card-text text-truncate font-weight-bold text-success"
+        >NT$ {{ product.sell_price }} 元</h5>
+        <div class="addToCart text-right py-5">
+          <button class="btn btn-danger px-5">加入購物車</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
