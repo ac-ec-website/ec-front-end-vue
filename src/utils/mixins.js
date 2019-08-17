51351d72333a183e579f05e252641049d2e15629
wanaka <asd8116@gmail.com>
2019-08-17 13:31:25 +0800

    API integration of AdminOrders page

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -8,6 +8,26 @@ export const fromNowFilter = {
   }
 }
 
+export const dateTimeFilter = {
+  filters: {
+    dateTime(time) {
+      return time ? moment(time).format('YYYY-MM-DD HH:mm') : '-'
+    }
+  }
+}
+
+export const currencyFilter = {
+  filters: {
+    currency(num) {
+      const n = Number(num)
+      return `$${n.toFixed(0).replace(/./g, (c, i, a) => {
+        const currency = i && c !== '.' && (a.length - i) % 3 === 0 ? `, ${c}`.replace(/\s/g, '') : c
+        return currency
+      })}`
+    }
+  }
+}
+
 export const emptyImageFilter = {
   filters: {
     emptyImage(src) {
--- src/views/admin/AdminOrders.vue
+++ src/views/admin/AdminOrders.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="container py-5">
     <AdminNav />
+
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th scope="col">購買時間</th>
+          <th scope="col">購買者</th>
+          <th scope="col">購買款項</th>
+          <th scope="col">應付金額</th>
+          <th scope="col">是否付款</th>
+          <th scope="col">訂單詳情</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="order in orders" :key="order.id">
+          <th scope="row">{{ order.updatedAt | dateTime }}</th>
+          <td>{{ order.name }}</td>
+
+          <td>
+            <ul class="list-unstyled">
+              <li
+                v-for="product in order.items"
+                :key="product.id"
+              >{{ product.name }} QT：{{ product.OrderItem.quantity }}</li>
+            </ul>
+          </td>
+
+          <td class="text-right">{{ order.total_amount | currency }}</td>
+
+          <td>
+            <strong v-if="order.payment_status === 1" class="text-success">已付款</strong>
+            <span v-else class="text-muted">尚未付款</span>
+          </td>
+
+          <td>
+            <router-link
+              :to="{name: 'admin-order', params: {orderId: order.id}}"
+              class="btn btn-link"
+            >Show</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 import AdminNav from '@/components/admin/AdminNav'
 
+import { dateTimeFilter, currencyFilter } from '@/utils/mixins'
 import { Toast } from '@/utils/helpers'
 
 export default {
   components: {
     AdminNav
   },
+  mixins: [dateTimeFilter, currencyFilter],
   data() {
     return {
       orders: []
