25339cb14c978cbf22c895cccc2d1dca989f80b0
pierce <pierceshih@gmail.com>
2019-08-31 14:39:11 +0800

    Refactor for the ProductsPage and ProductPage

---
+++ src/apis/products.js
@@ -0,0 +1,17 @@
+import {
+  apiHelper
+} from './../utils/helpers'
+
+export default {
+  getProducts() {
+    return apiHelper.get(`/products`)
+  },
+  getProduct(productId) {
+    return apiHelper.get(`/products/${productId}`)
+  },
+  deleteItem(
+    cartId, cartItemId
+  ) {
+    return apiHelper.delete(`/cart/${cartId}/cartItem/${cartItemId}`)
+  },
+}
--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -42,7 +42,6 @@
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   props: {
     initialProduct: {
