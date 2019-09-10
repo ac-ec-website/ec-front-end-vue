50584ffce41018808b9b9df48a2e3c2525970e81
pierce <pierceshih@gmail.com>
2019-09-11 02:22:09 +0800

    Add getCoupon and deleteCoupon features to couponAPI

--- src/apis/coupons.js
+++ src/apis/coupons.js
@@ -4,8 +4,12 @@ import {
 
 export default {
   postCoupon(couponCode) {
-    return apiHelper.post('/coupon',
-      couponCode
-    )
+    return apiHelper.post('/coupon', couponCode)
+  },
+  getCoupon() {
+    return apiHelper.get('/coupon')
+  },
+  deleteCoupon(couponCode) {
+    return apiHelper.delete('/coupon', couponCode)
   },
 }
