a5a24e6f104a1bb055bbbeca8e84a5d350e26079
wanaka <asd8116@gmail.com>
2019-10-09 11:03:21 +0800

    Modify admin views bootstrap

--- src/views/admin/AdminDiscounts.vue
+++ src/views/admin/AdminDiscounts.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <AdminNav class="sidebar col-md-2 d-none d-md-block bg-light" />
 
-    <div class="container col-md-9 ml-sm-auto col-lg-10 px-4">
+    <div class="container col-md-9 ml-sm-auto col-lg-10">
       <router-link to="/admin/discounts/new" class="btn btn-primary btn-block mb-4">
         New Discounts
         <i class="fas fa-plus-square"></i>
